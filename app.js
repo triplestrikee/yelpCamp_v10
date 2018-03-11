ccafb26ba186aa5f22cdd47fde430b00d870955b
Han <shenhanaholic@gmail.com>
2018-03-11 21:05:58 +0000

    change app.js db to mlab

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
     
-mongoose.connect("mongodb://localhost/yelp_camp_v10");
-//mongoose.connect("mongodb://han:Hans_9oo916@ds163918.mlab.com:63918/yelpcamp");
+//mongoose.connect("mongodb://localhost/yelp_camp_v10");
+mongoose.connect("mongodb://han:Hans_9oo916@ds163918.mlab.com:63918/yelpcamp");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
