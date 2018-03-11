d05147a22af0b3425bd8af3fdea6cb2746653edf
Han <shenhanaholic@gmail.com>
2018-03-11 02:07:26 +0000

    add mongolab

--- app.js
+++ app.js
@@ -14,7 +14,9 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
     
-mongoose.connect("mongodb://localhost/yelp_camp_v10");
+//mongoose.connect("mongodb://localhost/yelp_camp_v10");
+mongoose.connect("mongodb://han:Hans_9oo916@ds163918.mlab.com:63918/yelpcamp");
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
